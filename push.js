410cd3fbee76a435f5a3e27addb034158f7be444
Fran Domovic <fd52663@fer.hr>
2022-01-04 15:07:12 +0100

    fix

--- push.js
+++ push.js
@@ -6,5 +6,5 @@ let vapidKeys = {
   }
 
   push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
-  let sub={};
+  let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/eFFXjW1EfIw:APA91bGHS6aVgw84bnMyWIfx8T-aVzDcwoXcGWNIL4SMeJscQ8d83nCRDCBQh0RNghvs_Xzj4SMSDDLEk-bbvgySxw8uyv5j-SfznD5mLr_yVaxfccvXHQTKD5Os8G98e7twBEtYAryO","expirationTime":null,"keys":{"p256dh":"BJ33mzhqI4omo3q2w6iHIzJtU6fkJexXCdaPQejObSNm-nzS6MyI9SowYhI-4-KwpINw_bCUoc7h70Sz1zdV_EU","auth":"xjTZV6xQiR4sKCmAH3k7Mg"}};
   push.sendNotification(sub ,'text message')
--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('push', () => {
-    self.ServiceWorkerRegistration.sendNotification('test message', {})
+self.addEventListener('push', function(e) {
+    var options = {
+        body: 'This notification was generated',
+        vibrate: [100, 50, 100],
+        date: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {action: 'explore', title: 'Explore this new world',
+            icon: 'images/checkmark.png'},
+            {action: 'close', title: 'Close',
+                icon: 'images/xmark.png'},
+        ]
+        
+    };
+    e.waitUntil(
+        self.ServiceWorkerRegistration.showNotification('hello ',options)
+    )
 });
